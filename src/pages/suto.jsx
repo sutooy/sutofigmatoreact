f401a894fe26df5d9bae1046e857559a74e7bef1
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2021-07-24 18:54:45 +0700

    sutoFigma

--- src/pages/suto.jsx
+++ src/pages/suto.jsx
@@ -13,7 +13,7 @@ return (
 <>
 
 
-  <div className="FrameContainer">
+  <div className="FrameContainer" id="main">
     {/* <h1>test test</h1> */}
     <div className="LeftContainer">
       <img src="https://i.pinimg.com/originals/29/06/49/29064960c2b0e3b0ab3fa1e9005ee971.jpg" alt="foto gunung"></img>
@@ -29,8 +29,11 @@ return (
           witness beauty.</p>
       </div>
 
-      <div className="Small Box">
-        
+      <div className="SmallBox">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Eo_circle_grey_arrow-right.svg/768px-Eo_circle_grey_arrow-right.svg.png" alt="arrow"></img>
+      {/* <img src="https://lh3.googleusercontent.com/proxy/8QjJNSImsNvh6Eyrih4jRfPj4Issw5YFUVf5PxRvs2-FJBpYicXGTgk8LaRVQf26_SIni2UH-GdKm_VcTsKmB5G13axNSto8cBVk9tRRHse9" alt="arrow"></img> */}
+      {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/VisualEditor_-_Icon_-_External-link.svg/768px-VisualEditor_-_Icon_-_External-link.svg.png" alt="arrow"></img> */}
+      
       </div>
 
 
