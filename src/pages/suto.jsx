5163fe0e439b98b48d53044b45f7a4ec9e7aa2c8
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2021-07-25 14:17:26 +0700

    sutoUpdate

--- src/pages/suto.jsx
+++ src/pages/suto.jsx
@@ -1,5 +1,7 @@
 import "./suto.css";
 import React from 'react'
+import img from "./Screenshot from 2021-07-25 11-47-09.png"
+import arrow from './arrow1.png'
 
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
@@ -16,9 +18,13 @@ return (
   <div className="FrameContainer" id="main">
     {/* <h1>test test</h1> */}
     <div className="LeftContainer">
-      <img src="https://i.pinimg.com/originals/29/06/49/29064960c2b0e3b0ab3fa1e9005ee971.jpg" alt="foto gunung"></img>
+      <img src={img} alt="foto gunung"></img>
     </div>
 
+    {/* <div className="LeftContainer">
+      <img src="https://i.pinimg.com/originals/29/06/49/29064960c2b0e3b0ab3fa1e9005ee971.jpg" alt="foto gunung"></img>
+    </div> */}
+    {/* ../../public/Screenshot from 2021-07-25 11-47-09.png */}
     <div className="RightContainer">
       <div className="Title">
        <p>An Above Average Experience</p>
@@ -30,7 +36,11 @@ return (
       </div>
 
       <div className="SmallBox">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Eo_circle_grey_arrow-right.svg/768px-Eo_circle_grey_arrow-right.svg.png" alt="arrow"></img>
+      {/* <i class="fas fa-external-link-square"></i>      */}
+       {/* <img src="fas fa-external-link-square" alt="arrow"></img> */}
+       <img src={arrow} alt="arrow"></img>
+
+      {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Eo_circle_grey_arrow-right.svg/768px-Eo_circle_grey_arrow-right.svg.png" alt="arrow"></img> */}
       {/* <img src="https://lh3.googleusercontent.com/proxy/8QjJNSImsNvh6Eyrih4jRfPj4Issw5YFUVf5PxRvs2-FJBpYicXGTgk8LaRVQf26_SIni2UH-GdKm_VcTsKmB5G13axNSto8cBVk9tRRHse9" alt="arrow"></img> */}
       {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/VisualEditor_-_Icon_-_External-link.svg/768px-VisualEditor_-_Icon_-_External-link.svg.png" alt="arrow"></img> */}
       
